435297652edfc482102d836f2d9b571237feebf6
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2021-02-04 12:52:41 +0100

    [TEST] Add MLP classification/regression test settings

---
+++ test/settings.py
@@ -0,0 +1,32 @@
+from test.utils import classification_targets, regression_targets
+
+import torch
+
+SETTINGS = [
+    # classification
+    {
+        "input_fn": lambda: torch.rand(3, 10),
+        "module_fn": lambda: torch.nn.Sequential(
+            torch.nn.Linear(10, 7), torch.nn.Linear(7, 5)
+        ),
+        "loss_function_fn": lambda: torch.nn.CrossEntropyLoss(reduction="mean"),
+        "target_fn": lambda: classification_targets((3,), 5),
+    },
+    {
+        "input_fn": lambda: torch.rand(3, 10),
+        "module_fn": lambda: torch.nn.Sequential(
+            torch.nn.Linear(10, 7), torch.nn.ReLU(), torch.nn.Linear(7, 5)
+        ),
+        "loss_function_fn": lambda: torch.nn.CrossEntropyLoss(reduction="sum"),
+        "target_fn": lambda: classification_targets((3,), 5),
+    },
+    # Regression
+    {
+        "input_fn": lambda: torch.rand(3, 10),
+        "module_fn": lambda: torch.nn.Sequential(
+            torch.nn.Linear(10, 7), torch.nn.Sigmoid(), torch.nn.Linear(7, 5)
+        ),
+        "loss_function_fn": lambda: torch.nn.MSELoss(reduction="mean"),
+        "target_fn": lambda: regression_targets((3, 5)),
+    },
+]
