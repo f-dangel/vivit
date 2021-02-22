0e55118e36d6d720a6932d1cee83bd835dd9b50c
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2021-02-22 10:37:27 +0100

    [REF] Make tests, examples, and experiments share filtering of nonzero EVals

--- test/extensions/secondorder/sqrt_ggn/test_gram_sqrt_ggn.py
+++ test/extensions/secondorder/sqrt_ggn/test_gram_sqrt_ggn.py
@@ -4,9 +4,11 @@ from test.implementation.autograd import AutogradExtensions
 from test.implementation.backpack import BackpackExtensions
 from test.problem import make_test_problems
 from test.settings import SETTINGS
-from test.utils import check_sizes_and_values, remove_zeros
+from test.utils import check_sizes_and_values
 
 import pytest
+
+from lowrank.utils.eig import symeig
 
 PROBLEMS = make_test_problems(SETTINGS)
 IDS = [problem.make_id() for problem in PROBLEMS]
@@ -20,17 +22,12 @@ def test_gram_sqrt_ggn_spectrum(problem):
     ggn_mat = AutogradExtensions(problem).ggn()
     gram_mat = BackpackExtensions(problem).gram_sqrt_ggn()
 
-    ggn_evals, _ = ggn_mat.symeig()
-    gram_evals, _ = gram_mat.symeig()
-
     rtol, atol = 1e-5, 1e-6
-    filtered_ggn_evals = remove_zeros(ggn_evals, rtol=rtol, atol=atol)
-    filtered_gram_evals = remove_zeros(gram_evals, rtol=rtol, atol=atol)
+    ggn_evals, _ = symeig(ggn_mat, atol=atol, rtol=rtol)
+    gram_evals, _ = symeig(gram_mat, atol=atol, rtol=rtol)
 
     rtol, atol = 1e-5, 1e-7
-    check_sizes_and_values(
-        filtered_ggn_evals, filtered_gram_evals, rtol=rtol, atol=atol
-    )
+    check_sizes_and_values(ggn_evals, gram_evals, rtol=rtol, atol=atol)
     problem.tear_down()
 
 
@@ -51,17 +48,12 @@ def test_gram_sqrt_ggn_mc_spectrum(problem, mc_samples):
     gram_mat = BackpackExtensions(problem).gram_sqrt_ggn_mc(mc_samples)
     problem.tear_down()
 
-    ggn_evals, _ = ggn_mat.symeig()
-    gram_evals, _ = gram_mat.symeig()
-
     rtol, atol = 1e-5, 1e-6
-    filtered_ggn_evals = remove_zeros(ggn_evals, rtol=rtol, atol=atol)
-    filtered_gram_evals = remove_zeros(gram_evals, rtol=rtol, atol=atol)
+    ggn_evals, _ = symeig(ggn_mat, atol=atol, rtol=rtol)
+    gram_evals, _ = symeig(gram_mat, atol=atol, rtol=rtol)
 
     rtol, atol = 1e-5, 1e-7
-    check_sizes_and_values(
-        filtered_ggn_evals, filtered_gram_evals, rtol=rtol, atol=atol
-    )
+    check_sizes_and_values(ggn_evals, gram_evals, rtol=rtol, atol=atol)
 
 
 FREE_SQRT_GGN = [True, False]
