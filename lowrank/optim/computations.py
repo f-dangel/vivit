fix: Wrong normalization constant of eigenvectors in γ computation

lowrank/optim/computations.py
@@ -230,7 +230,7 @@ class BaseComputations:
         V_t_g_n = self._V_t_mat_prod[group_id](g_n, flatten=True)
         gammas = (
             torch.einsum("ni,id->nd", V_t_g_n, self._gram_evecs[group_id])
-            / self._gram_evals[group_id]
+            / self._gram_evals[group_id].sqrt()
         )
 
         self._gammas[group_id] = gammas

test/implementation/autograd.py
@@ -279,14 +279,13 @@ class AutogradExtensions(ExtensionsImplementation):
         if ggn_subsampling is not None:
             ggn *= N / len(ggn_subsampling)
 
-        evals, evecs = ggn.symeig(eigenvectors=True)
+        _, evecs = ggn.symeig(eigenvectors=True)
 
         # select top eigenspace
         num_evecs = int(
             top_space * self._ggn_num_nontrivial_evals(subsampling=ggn_subsampling)
         )
         num_evecs = max(num_evecs, 1)
-        evals = evals[-num_evecs:]
         evecs = evecs[:, -num_evecs:]
 
         grad_batch = self.batch_grad(subsampling=grad_subsampling)
@@ -298,7 +297,7 @@ class AutogradExtensions(ExtensionsImplementation):
             [g.flatten(start_dim=1) for g in individual_gradients], dim=1
         )
 
-        gammas = torch.einsum("ni,id->nd", individual_gradients, evecs) / evals.sqrt()
+        gammas = torch.einsum("ni,id->nd", individual_gradients, evecs)
 
         return gammas
 

test/implementation/backpack.py
@@ -328,7 +328,7 @@ class BackpackExtensions(ExtensionsImplementation):
         if ggn_subsampling is not None:
             V_t_g *= math.sqrt(N / len(ggn_subsampling))
 
-        gammas = torch.einsum("ni,id->nd", V_t_g, evecs) / evals
+        gammas = torch.einsum("ni,id->nd", V_t_g, evecs) / evals.sqrt()
 
         return gammas
 