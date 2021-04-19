419be3c484b239d8d5054ee4300d9d432a5cd514
Lukas Tatzel <lukas-nicola.tatzel@uni-tuebingen.de>
2021-04-19 16:48:01 +0200

    [FIX] Fixed garglint issues

--- lowrank/utils/logging.py
+++ lowrank/utils/logging.py
@@ -6,8 +6,12 @@ GLOBAL_STEP = 0
 
 def get_logger():
     """Get the global ``LOGGER``. If ``LOGGER`` is ``None``, initialize the logger
-    by calling the ``init_logger`` function"""
+    by calling the ``init_logger`` function.
 
+    Returns:
+        logging.Logger: The ``logging.Logger`` object created by init_logger and stored
+            in the global variable ``LOGGER``
+    """
     global LOGGER
     if LOGGER is None:
         LOGGER = init_logger()
@@ -15,7 +19,11 @@ def get_logger():
 
 
 def init_logger():
-    """Initialize the a logger``."""
+    """Initialize the logger.
+
+    Returns:
+        logging.Logger: The logger with the configuration specified below
+    """
 
     # Create logger
     logger = logging.getLogger("lowrank")
@@ -37,24 +45,42 @@ def init_logger():
 
 
 def set_global_step(new_step):
-    """Set the global step to the given ``new_step``"""
+    """Set the global step to the given ``new_step``
+
+    Args:
+        new_step (int): Set the global logging step ``GLOBAL_STEP`` to ``new_step``
+    """
     global GLOBAL_STEP
     GLOBAL_STEP = new_step
 
 
 def get_global_step():
-    """Return the global step"""
+    """Return the global step
+
+    Returns:
+        int: The current global logging step ``GLOBAL_STEP``
+    """
     return GLOBAL_STEP
 
 
 def log_with_global_step(msg):
-    """Write a log message including the global step."""
+    """Write a log message including the global logging step.
 
+    Args:
+        msg (str): The logging message
+    """
     msg_with_global_step = f"global_step={get_global_step()}:{msg}"
     logger = get_logger()
     logger.info(msg_with_global_step)
 
 
-def tensor_to_list(tensor):
-    """Convert a torch.Tensor to a list"""
-    return tensor.detach().cpu().numpy().tolist()
+def tensor_to_list(T):
+    """Convert a torch.Tensor to a list
+
+    Args:
+        T (torch.Tensor): The ``torch.Tensor`` that is supposed to be converted
+
+    Returns:
+        list: The tensor converted to a list
+    """
+    return T.detach().cpu().numpy().tolist()
