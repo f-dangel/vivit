89eef8db9b4e9ccb267f54093b977ed8e325bdbc
Felix Dangel <fdangel@tuebingen.mpg.de>
2021-05-05 09:48:42 +0200

    feature(eig): Implement stable symeig for PSD matrices

--- lowrank/utils/eig.py
+++ lowrank/utils/eig.py
@@ -3,6 +3,7 @@
 import torch
 
 
+# TODO Deprecate this method in favor of symeig_psd
 def stable_symeig(input, eigenvectors=False, upper=True):
     """Compute EVals and EVecs of a matrix.
 
@@ -27,37 +28,80 @@ def stable_symeig(input, eigenvectors=False, upper=True):
         RuntimeError: If solver did not converge or if input or output tensor contains
             ``nan``s
     """
+    return symeig_psd(
+        input, eigenvectors=eigenvectors, upper=upper, shift=1.0, shift_inplace=False
+    )
+
+
+def symeig_psd(input, eigenvectors=False, upper=True, shift=0.0, shift_inplace=False):
+    """Compute EVals and EVecs of a positive semi-definite symmetric matrix.
+
+    This is a wrapper around ``torch.symeig``. It shifts the input's diagonal to
+    improve its condition number. This avoids convergence problems with
+    ``torch.symeig`` for ill-conditioned positive semi-definite matrices.
+
+    Args:
+        input (torch.Tensor): 2d symmetric tensor.
+        eigenvectors (bool): Whether eigenvectors should be computed.
+        upper(bool): Whether to consider upper-triangular or lower-triangular
+            region of the matrix.
+        shift (float, optional): The shift applied to the diagonal of ``input``.
+            Default value: ``0.0``.
+        shift_inplace (bool, optional): Shift the input inplace. If ``False``, copy the
+            input before shifting. Default value: ``False``.
+
+    Returns:
+        (torch.Tensor, torch.Tensor): First tensor of one dimension contains
+            eigenvalues. Second tensor holds associated eigenvectors stored columnwise,
+            i.e. ``evecs[:, i]`` is eigenvector with eigenvalue ``evals[i]``.
+
+    Raises:
+        ValueError: If ``input`` does not have dimension 2.
+        RuntimeError: If solver did not converge or input contains ``nan``s.
+    """
     if input.dim() != 2:
-        raise ValueError("Input must be of dimension 2")
-    if input.shape[0] != input.shape[1]:
-        raise ValueError("Input must be square")
-    if _has_nans(input):
-        raise RuntimeError("Input has nans")
+        raise ValueError(f"Input must have dimension 2. Got {input.dim()}.")
 
-    def shift_diag(input, shift):
-        """Shifts the diagonal of the square ``input`` tensor by ``shift``
-
-        Args:
-            input (torch.Tensor): 2d square tensor.
-            shift (float): The shift applied to the diagonal of ``input``
-
-        Returns:
-            torch.Tensor: input with shifted diagonal
-        """
-        return input + torch.diag(shift * torch.ones(input.shape[0])).to(input.device)
+    input = shift_diag(input, shift, inplace=shift_inplace)
 
     try:
         evals, evecs = input.symeig(eigenvectors=eigenvectors, upper=upper)
-        return evals, evecs
-    except RuntimeError:
-        SHIFT = 1.0
-        shifted_input = shift_diag(input, SHIFT)
-        try:
-            evals, evecs = shifted_input.symeig(eigenvectors=eigenvectors, upper=upper)
-        except RuntimeError as e:
-            e_msg = getattr(e, "message", repr(e))
-            raise RuntimeError(f"{e_msg}")
-        return evals - SHIFT, evecs
+    except RuntimeError as e:
+        e_msg = getattr(e, "message", repr(e))
+        raise RuntimeError(f"{e_msg} Tensor contains NaNs: {_has_nans(input)}")
+
+    # shift back
+    if shift_inplace:
+        input = shift_diag(input, -shift, inplace=shift_inplace)
+
+    evals -= shift
+
+    return evals, evecs
+
+
+def shift_diag(input, shift, inplace=False):
+    """Shifts the diagonal of the square ``input`` tensor by ``shift``.
+
+    Args:
+        input (torch.Tensor): 2d tensor.
+        shift (float): The shift applied to the diagonal of ``input``
+        inplace (bool, optional): Modify the tensor inplace. If ``False``, copy the
+            input before shifting. Default value: ``False``.
+
+    Returns:
+        torch.Tensor: Input with shifted diagonal.
+    """
+    if shift == 0.0:
+        return input
+
+    if inplace:
+        result = input
+    else:
+        result = input.clone()
+
+    result[range(input.shape[0]), range(input.shape[1])] += shift
+
+    return result
 
 
 def symeig(input, eigenvectors=False, upper=True, atol=1e-7, rtol=1e-5):
