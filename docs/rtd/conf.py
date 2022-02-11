5e406a81c7c0aac30a451d2c574f4eeeec47bc3a
Felix Dangel <fdangel@tue.mpg.de>
2022-02-11 11:18:59 +0100

    [DEL] Disable HTML theme and static path

--- docs/rtd/conf.py
+++ docs/rtd/conf.py
@@ -18,7 +18,6 @@
 # -- Project information -----------------------------------------------------
 
 project = "ViViT"
-
 copyright = "2022, F. Dangel, L. Tatzel"
 author = "F. Dangel, L. Tatzel"
 
@@ -47,9 +46,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+# html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
