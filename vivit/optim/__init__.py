3a98c90cbafc975b504edeaf140d6396efda5da3
Felix Dangel <fdangel@tue.mpg.de>
2022-02-14 21:51:01 +0100

    [DEL] Remove Newton step and Newton optimizer
    Currently, the Newton step depends on the directional derivatives.
    I will be easier to clean up the latter's API before reactivating the
    Newton step feature.

--- vivit/optim/__init__.py
+++ vivit/optim/__init__.py
@@ -1,13 +1,7 @@
 """Optimization methods using low-rank representations of the GGN/Fisher."""
 
-from vivit.optim.computations import BaseComputations
-from vivit.optim.damped_newton import DampedNewton
-from vivit.optim.damping import ConstantDamping
 from vivit.optim.directional_derivatives import DirectionalDerivativesComputation
 
 __all__ = [
-    "DampedNewton",
-    "ConstantDamping",
-    "BaseComputations",
     "DirectionalDerivativesComputation",
 ]
